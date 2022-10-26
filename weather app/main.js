1f8950f32b1cc1486f67a8a820aa1bcc93b493ab
Sarah_techprep <116392309+Sarah-Tiana@users.noreply.github.com>
2022-10-26 00:52:41 -0500

    Add files via upload
    Figured out my bug and completed the program

--- weather app/main.js
+++ weather app/main.js
@@ -1,8 +1,6 @@
 const api = {
     key: "b46b852e783bf645cddc943ecbbf5bac",
-    baseurl: "https://api.openweathermap.org/data/2.5/"
-
-    //https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=b46b852e783bf645cddc943ecbbf5bac
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
@@ -11,17 +9,41 @@ searchbox.addEventListener('keypress', setQuery);
 function setQuery(evt) {
     if (evt.keyCode == 13) {
         getResults(searchbox.value);
-        //console.log(searchbox.value);
     }
 }
 
 function getResults (query) {
-    fetch('${api.base}weather?q=${query}&units=metric&APPID=${api.key}')
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`) //turns out the "`" is different from "'". this is where my error was coming from
         .then(weather => { return weather.json(); }).then(displayResults);
 }
 
 function displayResults (weather) {
     console.log(weather);
-    //let city = document.querySelector('.location .city');
-    //city.innerText = '${weather.name}, ${weather.sys.country}';
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`; //the back tick strikes again!!
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`
+}
+
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"];
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
 }
